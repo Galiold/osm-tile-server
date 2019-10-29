b5d2c3ef72bf2beead8295169d67e2a24a9fdaa1
Ali Goldani <a.goldani@mail.um.ac.ir>
2019-10-29 10:43:16 +0330

    file reader server added

--- index.js
+++ index.js
@@ -1,100 +1,95 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {MultiPoint, Point} from 'ol/geom';
+import {
+  MultiPoint,
+  Point
+} from 'ol/geom';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
-import {getVectorContext} from 'ol/render';
+import {
+  Circle as CircleStyle,
+  Fill,
+  Stroke,
+  Style
+} from 'ol/style';
+import {
+  getVectorContext
+} from 'ol/render';
+import XYZ from 'ol/source/XYZ'
+import { setInterval } from 'timers';
 
-function degrees2meters(lon,lat) {
-    var x = lon * 20037508.34 / 180;
-    var y = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
-    y = y * 20037508.34 / 180;
-    return [x, y]
-}
-
-function readTextFile(file) {
-    let coordinates = []
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                const arr = allText.split('\n')
-                const arrPair = arr.map(elem => {
-                    const pairStr = elem.split(',');
-                    const pairNum = pairStr.map(str => parseFloat(str));
-                    return pairNum;
-                })
-                const metersPair = arrPair.map(coord => degrees2meters(coord[0], coord[1]))
-                coordinates = metersPair;
-            }
-        }
-    }
-    rawFile.send(null);
-    return coordinates;
+function httpGetAsync(theUrl, callback) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+  xmlHttp.send(null);
 }
 
 var tileLayer = new TileLayer({
-  source: new OSM({ url: 'http://185.252.28.133/hot/{z}/{x}/{y}.png' })
+  source: new XYZ({
+    url: 'http://localhost/styles/osm-bright/{z}/{x}/{y}.png' +
+      '?apiKey=WyI2YzM4YTAzNS01MGRiLTQ1YzMtOTRjNC03YmRlYjE0ZjA4MmIiLCItMSIsODEyN10.XbcggA.r0pUnagLql-0daR8BFDW68pYKmE'
+  })
 });
 
 var map = new Map({
   layers: [tileLayer],
   target: 'map',
   view: new View({
-    center: [6083756.846017, 4024218.446644],
-    zoom: 5
+    center: [6636396.549632117, 4341896.0510656107],
+    zoom: 13
   })
 });
 
 var imageStyle = new Style({
   image: new CircleStyle({
     radius: 5,
-    fill: new Fill({color: 'yellow'}),
-    stroke: new Stroke({color: 'blue', width: 3})
+    fill: new Fill({
+      color: 'yellow'
+    }),
+    stroke: new Stroke({
+      color: 'blue',
+      width: 3
+    })
   })
 });
 
 var headInnerImageStyle = new Style({
   image: new CircleStyle({
     radius: 2,
-    fill: new Fill({color: 'blue'})
+    fill: new Fill({
+      color: 'blue'
+    })
   })
 });
 
 var headOuterImageStyle = new Style({
   image: new CircleStyle({
     radius: 5,
-    fill: new Fill({color: 'black'})
+    fill: new Fill({
+      color: 'black'
+    })
   })
 });
 
-var n = 100;
-var omegaTheta = 30000; // Rotation period in ms
-var R = 7e6;
-var r = 2e6;
-var p = 2e6;
-tileLayer.on('postrender', function(event) {
+let coordinates = []
+
+setInterval(() => {
+  httpGetAsync('http://localhost:3000/coordinates', (response) => {
+    const body = JSON.parse(response)
+    coordinates = body.result
+    map.render()
+  })
+}, 1000);
+
+tileLayer.on('postrender', function (event) {
+  if (coordinates) {
+    
   var vectorContext = getVectorContext(event);
-//   var frameState = event.frameState;
-//   var theta = 2 * Math.PI * frameState.time / omegaTheta;
-//   var coordinates = [];
-//   var i;
-//   for (i = 0; i < n; ++i) {
-//     // var t = theta + 2 * Math.PI * i / n;
-//     // var x = (R + r) * Math.cos(t) + p * Math.cos((R + r) * t / r);
-//     // var y = (R + r) * Math.sin(t) + p * Math.sin((R + r) * t / r);
-//     var x = 100000 * i;
-//     coordinates.push([x, 100]);
-//   }
-  const coordinates = readTextFile(require('./test.txt'))
-  
   vectorContext.setStyle(imageStyle);
   vectorContext.drawGeometry(new MultiPoint(coordinates));
 
@@ -105,9 +100,7 @@ tileLayer.on('postrender', function(event) {
 
   vectorContext.setStyle(headInnerImageStyle);
   vectorContext.drawGeometry(headPoint);
-
-  //   map.render();
+  }
 });
 
-// readTextFile(require('./test.txt'))
 map.render();
---
+++ reader.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const fs = require('fs')
+
+app.get('/coordinates', (req, res) => {
+  readCoords()
+    .then(coords => {
+      res.status = 200
+      res.send({
+        status: 'Ok',
+        result: coords
+      })
+    })
+    .catch(err => {
+      res.status = 500
+      res.send({
+        status: 'Error',
+        result: err
+      })
+    })
+
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Reader listining on port ${PORT}.`)
+})
+
+const readCoords = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./test.txt', (err, data) => {
+      if (err) {
+        reject(new Error(err))
+      } else {
+        const dataStr = data.toString()
+        const dataArr = dataStr.split('\r\n')
+        const dataPairArr = dataArr.map(pair => {
+          const pairStr = pair.split(',')
+          const pairFloat = pairStr.map(str => parseFloat(str))
+          return pairFloat
+        })
+        const lonLat = dataPairArr.map(pair => degrees2meters(pair[0], pair[1]))
+        resolve(lonLat)
+      }
+    })
+  })
+}
+
+const degrees2meters = (lon, lat) => {
+  var x = lon * 20037508.34 / 180;
+  var y = Math.log(Math.tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
+  y = y * 20037508.34 / 180;
+  return [x, y]
+}
